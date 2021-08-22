968fae84520d25e65002f63c121cddd3df9a446a
KentaItakura <kentai@scanx.com>
2021-08-22 14:22:17 +0900

    doc: readme updated

--- src/CNN_classification_using_random_erasing_and_cut_out.m
+++ src/CNN_classification_using_random_erasing_and_cut_out.m
@@ -1,54 +1,66 @@
-[![View CNN-classification-using-random-erasing-and-cut-out on File Exchange](https://www.mathworks.com/matlabcentral/images/matlab-file-exchange.svg)](https://jp.mathworks.com/matlabcentral/fileexchange/98034-cnn-classification-using-random-erasing-and-cut-out)
-# CNN classification using random erasing / cut out
+%% CNN classification using random erasing / cut out
+% *[English]*
+% 
+% This demo shows how to perform random erasing/cut out augmentation in CNN 
+% classification as explained in [1] and [2]. A rectangle mask was created randomly 
+% on the training images to escape overfitting as shown below. In this demo, a 
+% gray color mask was made and the height and width ranged from 1 to the half 
+% of the image size. The color and size of the mask can be changed in the custom 
+% function at the end of this script. 
+% 
+% The test accuracy with/without the random erasing/cut out was compared; the 
+% test accucacy with the technique was significantly higher tan that without the 
+% random erasing/cut out. Because the network with it, to some extent, escape 
+% the over-fitting to the training data. This was done with cifar-10 dataset. 
+% You may alleviate the over-fitting using the random erasing/cut out. 
+% 
+% *[Japanese]*
+% 
+% このファイルでは、cut outやrandom erasingとよばれる方法 [1, 2] を用いてデータ拡張をし、さらに畳み込みニューラルネットワーク（CNN)にて分類をする例を示します。過学習を防ぐために、訓練データセットの画像にランダムに長方形のマスクをかけ、その画像をもとに学習を行います。Cifar-10とよばれるデータセット 
+% [3, 4]を用いてこのデータ拡張によって、分類精度が上昇することを確認できます。輝度値が128のグレーのマスクをかけていますが、訓練データ内の輝度値の平均を使ったり、ゴマ塩ノイズを乗せたりと工夫をすることでさらなる精度上昇の可能性も考えられます。
+% 
+% *[Key words]*
+% 
+% augmentation, cifar-10, classification, cnn, cut out, data augmentation, over-fitting, 
+% over-tune, random erasing
+% 
+% 
+% 
+% [1] <https://arxiv.org/abs/1708.04896 Zhong, Z., L. Zheng, G. Kang, S. Li, 
+% and Y. Yang. "Random erasing data augmentation. arXiv 2017." arXiv preprint 
+% arXiv:1708.04896.>
+% 
+% [2] <https://arxiv.org/abs/1708.04552 DeVries, Terrance, and Graham W. Taylor. 
+% "Improved regularization of convolutional neural networks with cutout." arXiv 
+% preprint arXiv:1708.04552 (2017).>
+% 
+% [3] Krizhevsky, A., and G. Hinton. "Learning multiple layers of features from 
+% tiny images." Master's Thesis. University of Toronto, Toronto, Canada, 2009.
+% 
+% [4] The CIFAR-10 dataset (<https://www.cs.toronto.edu/~kriz/cifar.html https://www.cs.toronto.edu/~kriz/cifar.html>)
+%% Download and store the cifar-10 images
+% The helper function called downloadCIFARToFolders automatically save the cifar10 
+% images to the current directory. This helper function is provided by MathWorks 
+% (<https://jp.mathworks.com/help/deeplearning/examples/upload-deep-learning-data-to-the-cloud.html 
+% https://jp.mathworks.com/help/deeplearning/examples/upload-deep-learning-data-to-the-cloud.html>). 
+% This takes some time but, if the folder already exists, this step is skipped. 
 
-**[English]**
-
-This demo shows how to perform random erasing/cut out augmentation in CNN classification as explained in [1] and [2]. A rectangle mask was created randomly on the training images to escape overfitting as shown below. In this demo, a gray color mask was made and the height and width ranged from 1 to the half of the image size. The color and size of the mask can be changed in the custom function at the end of this script.  The test accuracy with/without the random erasing/cut out was compared; the test accucacy with the technique was significantly higher tan that without the random erasing/cut out. Because the network with it, to some extent, escape the over-fitting to the training data. This was done with cifar-10 dataset. You may alleviate the over-fitting using the random erasing/cut out. 
-
-**[Japanese]**
-
-このファイルでは、cut outやrandom erasingとよばれる方法 [1, 2] を用いてデータ拡張をし、さらに畳み込みニューラルネットワーク（CNN)にて分類をする例を示します。過学習を防ぐために、訓練データセットの画像にランダムに長方形のマスクをかけ、その画像をもとに学習を行います。Cifar-10とよばれるデータセット [3, 4]を用いてこのデータ拡張によって、分類精度が上昇することを確認できます。輝度値が128のグレーのマスクをかけていますが、訓練データ内の輝度値の平均を使ったり、ゴマ塩ノイズを乗せたりと工夫をすることでさらなる精度上昇の可能性も考えられます。
-
-**[Key words]**
-
-augmentation, cifar-10, classification, cnn, cut out, data augmentation, over-fitting, over-tune, random erasing
-
-![image_0.png](README_images/image_0.png)
-
-[1] [Zhong, Z., L. Zheng, G. Kang, S. Li, and Y. Yang. "Random erasing data augmentation. arXiv 2017." arXiv preprint arXiv:1708.04896.](https://arxiv.org/abs/1708.04896)
-
-[2] [DeVries, Terrance, and Graham W. Taylor. "Improved regularization of convolutional neural networks with cutout." arXiv preprint arXiv:1708.04552 (2017).](https://arxiv.org/abs/1708.04552)
-
-[3] Krizhevsky, A., and G. Hinton. "Learning multiple layers of features from tiny images." Master's Thesis. University of Toronto, Toronto, Canada, 2009.
-
-[4] The CIFAR-10 dataset (\href{https://www.cs.toronto.edu/~kriz/cifar.html}{https://www.cs.toronto.edu/\textasciitilde{}kriz/cifar.html})
-
-# Download and store the cifar-10 images
-
-The helper function called downloadCIFARToFolders automatically save the cifar10 images to the current directory. This helper function is provided by MathWorks ([https://jp.mathworks.com/help/deeplearning/examples/upload-deep-learning-data-to-the-cloud.html](https://jp.mathworks.com/help/deeplearning/examples/upload-deep-learning-data-to-the-cloud.html)). This takes some time but, if the folder already exists, this step is skipped. 
-
-```matlab:Code
 clear;clc;close all
 if exist('cifar10\')~=7
    [trainDirectory,testDirectory] = downloadCIFARToFolders(pwd);
 end
-```
+%% 
+% The |cifar10| images were stored in the imagedatastore. A portion of training 
+% dataset was used for validation image. 
 
-
-The `cifar10` images were stored in the imagedatastore. A portion of training dataset was used for validation image. 
-
-```matlab:Code
 cd('cifar10')
 imds = imageDatastore('train','IncludeSubfolders',true,'LabelSource','foldernames'); 
 [imdsTrain,imdsValidation] = splitEachLabel(imds,0.9);
 imdsTest = imageDatastore('test','IncludeSubfolders',true,'LabelSource','foldernames'); 
-```
+%% Randomly display images
+% Display some example of images. The augmented datastore returns images which 
+% are different at each time even with the identical index. 
 
-# Randomly display images
-
-Display some example of images. The augmented datastore returns images which are different at each time even with the identical index. 
-
-```matlab:Code
 % specify the number of images to display
 numDisplay=36;
 numTrain=numel(imdsTrain.Files);
@@ -62,15 +74,11 @@ for i=1:numDisplay
    subplot(6,6,i)
    imshow(Input);title(class{1})
 end
-```
+%% Define layers
+% The network consists of three layers. You can define each layer easily such 
+% as with |convolution2dLayer|.  For the detail, please check the comment below. 
+% Alternatively, you can define the CNN layer using a GUI tool called |deepNetworkDesigner|. 
 
-![figure_0.png](README_images/figure_0.png)
-
-# Define layers
-
-The network consists of three layers. You can define each layer easily such as with `convolution2dLayer`.  For the detail, please check the comment below. Alternatively, you can define the CNN layer using a GUI tool called `deepNetworkDesigner`. 
-
-```matlab:Code
 numFilters=75;
 layers = [
     % image input layer whose input size is 32-32-1. "3" represents one
@@ -98,19 +106,16 @@ layers = [
     fullyConnectedLayer(10)
     softmaxLayer
     classificationLayer];
-```
+%% 
+% Analyze the network architecture using |analyzeNetwork| function. If you would 
+% like to analyze it, please activate the following line.
 
-Analyze the network architecture using `analyzeNetwork` function. If you would like to analyze it, please activate the following line.
-
-```matlab:Code
 % analyzeNetwork(layers)
-```
+%% Display some masked images
+% To prepare the masked images, please use the |transform| function which transform 
+% images stored in the imagedatastore. The function to transform was customized 
+% using the custom function at the end of this script. 
 
-# Display some masked images
-
-To prepare the masked images, please use the `transform` function which transform images stored in the imagedatastore. The function to transform was customized using the custom function at the end of this script. 
-
-```matlab:Code
 miniBatchSize = 300;
 imdsTrain.ReadSize=miniBatchSize;
 imdsTrainTransformed = transform(imdsTrain,@CutOutAug,'IncludeInfo',true);
@@ -120,17 +125,9 @@ C=cell(12,1);
 idx=randperm(miniBatchSize,12);
 C=cat(4,I_transformed{idx(1:12),1});
 figure;montage(C)
-```
-
-![figure_1.png](README_images/figure_1.png)
-
-```matlab:Code
 reset(imdsTrainTransformed)
-```
+%% Specify the training options
 
-# Specify the training options
-
-```matlab:Code
 valFrequency = floor(numel(imdsTrain.Files)/miniBatchSize);
 options = trainingOptions('sgdm', ...
     'MiniBatchSize',miniBatchSize, ...
@@ -142,27 +139,17 @@ options = trainingOptions('sgdm', ...
     'ValidationFrequency',valFrequency, ...
     'Verbose',false, ...
     'Plots','training-progress');
-```
+%% Train the network
+% The training process was done with the |trainNetwork| function. 
 
-# Train the network
-
-The training process was done with the `trainNetwork` function. 
-
-```matlab:Code
 net = trainNetwork(imdsTrainTransformed,layers,options);
-```
+%% Accuracy assesment 
+% This section
+%% 
+% * classifies the test images
+% * calculate the overall accuracy
+% * display the confusion matrix
 
-![figure_2.png](README_images/figure_2.png)
-
-# Accuracy assesment 
-
-This section
-
-   -  classifies the test images 
-   -  calculate the overall accuracy 
-   -  display the confusion matrix 
-
-```matlab:Code
 % use classify function to classify the test data with the trained network
 YPred = classify(net,imdsTest);
 % YTest is the label information of the test data. 
@@ -170,28 +157,18 @@ YTest = (imdsTest.Labels);
 % if YPred (prediction result) and YTest (label data) is identical, it
 % returns 1, otherwise, 0. Its sum/number of test data is the accuracy
 accuracy = sum(YPred == YTest)/numel(YTest)
-```
-
-```text:Output
-accuracy = 0.7483
-```
-
-```matlab:Code
 % create confusion matrix
 confusionchart(YTest,YPred)
-```
+%% 
+% We show the training curve with the random erasing/cut out and without below. 
+% The test accuracy was 77.6 and 73.1, respectively. The network without the augmentation 
+% over-fitts to the training dataset, thus, the validation accuracy was lower. 
+% 
+% 
+%% Helper functions
+% The |CutOutAug| function creates the training images with the mask for random 
+% erasing/cut out. 
 
-![figure_3.png](README_images/figure_3.png)
-
-We show the training curve with the random erasing/cut out and without below. The test accuracy was 77.6 and 73.1, respectively. The network without the augmentation over-fitts to the training dataset, thus, the validation accuracy was lower. 
-
-![image_1.png](README_images/image_1.png)
-
-# Helper functions
-
-The `CutOutAug` function creates the training images with the mask for random erasing/cut out. 
-
-```matlab:Code
 function [dataOut,info] = CutOutAug(InputImgs,info)
 numImg = size(InputImgs,1);
 dataOut = cell(numImg,2);
@@ -215,11 +192,9 @@ for idx = 1:numImg
     dataOut(idx,:) = {I,info.Label(idx)};    
 end
 end
-```
+%% 
+% You can download the cifar10 data with |downloadCIFAR|ToFolders function. 
 
-You can download the cifar10 data with `downloadCIFAR`ToFolders function. 
-
-```matlab:Code
 function [trainDirectory,testDirectory] = downloadCIFARToFolders(directory)
 % This is a helper function to obtain CIFAR-10 and store it locally. 
 % The function copies the training and test data sets into a folder called "cifar10" in
@@ -296,4 +271,3 @@ fprintf('done.\n');
         end
     end
 end
-```
